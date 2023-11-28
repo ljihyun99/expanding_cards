9abfc0c2d921f2d736ec0bbfef0e9013c90996b8
jihyun <jihyun.lee@adesso.de>
2023-11-28 12:58:01 +0100

    added the second expanding cards for ocean

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ panels.forEach((panel)=>{
 
     })
 })
+
+const panels2= document.querySelectorAll('.panel2');
+panels2.forEach((panel)=>{
+    panel.addEventListener('click', ()=>{
+        panels2.forEach((panel)=>{
+            if(panel.classList.contains('active2')){
+                panel.classList.remove('active2');
+            }
+        })
+        panel.classList.add('active2');
+
+
+    })
+})
